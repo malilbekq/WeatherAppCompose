cdeb36cae662e66ca7b31f5039093b7ab76a5e2c
malilbek <helloworldestsenior@gmail.com>
2024-01-18 16:43:00 +0600

    WeatherApp v0.1

---
+++ app/src/main/java/com/example/weather/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.weather.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// light theme
+val Black = Color(0xFF000000)
+val SemiWhite = Color(0xFFEBDEDF)
+val Bord = Color(0xFF9A4A51)
+val WhiteGray = Color(0xFFC9D2CF)
+
+
+// dark theme
+val White = Color(0xFFFFFFFF)
+val WDarkGray = Color(0x802D2D2D)
+val VeryDarkGray = Color(0xFF222222)
+val Gray = Color(0x804D4D4D)
